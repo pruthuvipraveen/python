9f6913d4da1e81b726b4b1acad211beb688308e6
Pruthuvi Nugaliyadda <pruthuvipraveen@gmail.com>
2023-05-14 20:56:28 +0530

    More Basics

---
+++ exercise.py
@@ -0,0 +1,10 @@
+name = input("Please enter your name to proceed: ")
+
+print("Hey", name+",", "Welcome to Python!")
+
+num1 = int(input("Enter a value for number 1 : "))
+num2 = int(input("Enter a value for number 2 : "))
+
+sum = int(num1) + int(num2)
+
+print("Sum of two numbers:", sum)
